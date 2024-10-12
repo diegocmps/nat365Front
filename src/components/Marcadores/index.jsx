665a0fa8e7ef7d6bada2a2f6213d4c91c22d983c
diegocmps <diegobarreiros@hotmail.com>
2024-10-12 14:16:46 -0300

    ajuste nos marcadores do mapa (não estava renderizando após o deploy)

--- src/components/Marcadores/index.jsx
+++ src/components/Marcadores/index.jsx
@@ -1,5 +1,16 @@
 import PropTypes from 'prop-types';
 import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
+import markerIcon from '../../assets/imagens/leaf-green.png';
+import shadowIcon from '../../assets/imagens/leaf-shadow.png';
+
+const customMarkerIcon = new L.Icon({
+    iconUrl: markerIcon,
+    shadowUrl: shadowIcon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
 
 // eslint-disable-next-line react/prop-types
 export function Marcadores({ localidade }) {
@@ -9,6 +20,7 @@ export function Marcadores({ localidade }) {
                 <Marker
                     key={local.id}
                     position={[parseFloat(local.latitude), parseFloat(local.longitude)]}
+                    icon={customMarkerIcon}
                 >
                     <Popup>
                         <strong>{local.nome}</strong>
