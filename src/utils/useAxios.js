c9c49275f238f174d2681b7569f4e4ced57ab8ca
diegocmps <diegobarreiros@hotmail.com>
2024-11-01 20:26:02 -0300

    ajuste para deploy

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', 
-    // baseURL: 'https://m3p-backend-squad1-zwwd.onrender.com/', 
+    // baseURL: 'http://localhost:3000', 
+    baseURL: 'https://natureza365.onrender.com', 
 });
 
 api.interceptors.request.use(
