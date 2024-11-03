b995e0c174d4ecdc7dd88e2a27aa6c6071ede251
diegocmps <diegobarreiros@hotmail.com>
2024-11-03 11:29:23 -0300

    alteração baseURL

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', 
+    // baseURL: 'http://localhost:3000', 
 
-    // baseURL: 'https://natureza365.onrender.com', 
+    baseURL: 'https://natureza365.onrender.com', 
 });
 
 api.interceptors.request.use(
